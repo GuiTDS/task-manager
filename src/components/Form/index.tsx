e79097f11c3f3c61a6ab1b63ab5a906aa0c125ba
Guilherme Sanches <guitdsanches@gmail.com>
2023-06-06 16:03:13 -0300

    changing to module scss

--- src/components/Form/index.tsx
+++ src/components/Form/index.tsx
@@ -1,10 +1,10 @@
 import Button from "../Button";
-import './style.scss';
+import style from './Form.module.scss';
 
 const Form = () => {
     return (
-        <form className="novaTarefa">
-            <div className="inputContainer">
+        <form className={style.novaTarefa}>
+            <div className={style.inputContainer}>
                 <label htmlFor="tarefa">Adicione um novo estudo</label>
                 <input
                     type="text"
@@ -14,7 +14,7 @@ const Form = () => {
                     required
                 />
             </div>
-            <div className="inputContainer">
+            <div className={style.inputContainer}>
                 <label htmlFor="tempo">Tempo</label>
                     <input
                         type="time"
--- src/components/List/index.tsx
+++ src/components/List/index.tsx
@@ -1,4 +1,4 @@
-import './style.scss';
+import style from './List.module.scss';
 
 const List = () => {
 
@@ -10,11 +10,11 @@ const List = () => {
         tempo: '01:00:00'
     }]
     return (
-        <aside className='listaTarefas'>
+        <aside className={style.listaTarefas}>
             <h2>Estudos do dia</h2>
             <ul>
                 {tarefas.map((item, index) => (
-                    <li key={index} className='item'>
+                    <li key={index} className={style.item}>
                         <h3>{item.tarefa}</h3>
                         <span>{item.tempo}</span>
                     </li>
