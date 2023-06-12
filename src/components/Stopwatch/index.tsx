aff7a6955eb6fa5d998c8e9a7e8850023bd7d051
Guilherme Sanches <guitdsanches@gmail.com>
2023-06-12 16:23:44 -0300

    adding functionality to stopwatch

--- src/components/Stopwatch/index.tsx
+++ src/components/Stopwatch/index.tsx
@@ -1,11 +1,26 @@
+import { useEffect, useState } from 'react';
+import { tempoParaSegundos } from '../../common/utils/time';
+import ITarefa from '../../types/ITarefa';
 import Button from '../Button';
 import Clock from './Clock';
 import style from './Stopwatch.module.scss';
 
-const Stopwatch = () => {
+interface Props {
+    selecionado: ITarefa | undefined
+}
+
+const Stopwatch = ({selecionado}: Props) => {
+    const [tempo, setTempo] = useState<number>();
+
+    useEffect(() => {
+        if(selecionado?.tempo) {
+            setTempo(tempoParaSegundos(selecionado.tempo))
+        }
+    }, [selecionado])
     return ( 
         <div className={style.cronometro}>
             <p className={style.titulo}>Escolha um card e inicie o cronômetro</p>
+            Tempo: {tempo}
             <div className={style.relogioWrapper}>
                 <Clock />
             </div>
